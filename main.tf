7998e4e0175cf4248107b813fb1df319c8c04853
clement <cmrtzlff@gmail.com>
2020-12-14 12:32:02 +0100

    format main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "web" {
   ami                    = "ami-0bd39c806c2335b95"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
@@ -39,7 +39,7 @@ resource "aws_instance" "web" {
 
 
 resource "aws_security_group" "web-sg" {
-  name   = "${random_pet.sg.id}-sg"
+  name = "${random_pet.sg.id}-sg"
   ingress {
     from_port   = 80
     to_port     = 80
