71bd75f234af70d51fcb5885e1e4a6dd020140d8
clement <cmrtzlff@gmail.com>
2020-12-18 16:26:18 +0100

    fix(syntax): format main.tf

--- main.tf
+++ main.tf
@@ -22,9 +22,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0bd39c806c2335b95"
-  instance_type          = "t2.micro"
-  user_data = <<-EOF
+  ami           = "ami-0bd39c806c2335b95"
+  instance_type = "t2.micro"
+  user_data     = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
